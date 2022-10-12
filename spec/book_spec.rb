e8443dc8bb9891f7f134e4aac41a7716f5763e08
Htet Naing <htetnaing0814@gmail.com>
2022-10-12 11:07:52 +0630

    all tests for book, game and music

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,27 +1,27 @@
-require_relative '../label'
-require_relative '../book'
+require_relative './spec_helper'
 
 describe Book do
   before(:each) do
-    # @book = Book.new(publisher:'The Hobbit', published_date: '21-09-1937', )
+    @label = Label.new(title: 'Avatar', color: 'Blue')
+    @book = Book.new(publisher: 'Crunchyroll', cover_state: 'good', published_date: '18-03-2000', label: @label)
   end
 
   describe '#new' do
     context 'When creating a new book' do
-      it 'should return a Book object' do
+      it 'should return a book object' do
         expect(@book).to be_an_instance_of Book
       end
 
-      it 'should return a correct title' do
-        expect(@book.title).to eq 'The Hobbit'
-      end
-
-      it 'should return a correct author' do
-        expect(@book.author).to eq 'J.R.R. Tolkien'
-      end
-
       it 'should return a correct published_date' do
-        expect(@book.published_date).to eq '21-09-1937'
+        expect(@book.published_date).to eq '18-03-2000'
+      end
+
+      it 'should return a correct publisher' do
+        expect(@book.publisher).to eq 'Crunchyroll'
+      end
+
+      it 'should return a correct label' do
+        expect(@book.label).to eq @label
       end
     end
   end
